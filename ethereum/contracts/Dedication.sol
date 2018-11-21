c2dceb298ac78bafc071cf4c66696e4aa7ea9d4a
Drew Cannedy <dxc142830@utdallas.edu>
2018-11-20 21:19:58 -0600

    added contract tests

--- ethereum/contracts/Dedication.sol
+++ ethereum/contracts/Dedication.sol
@@ -1,31 +1,16 @@
 pragma solidity ^0.4.24;
 
 contract Dedication {
-    address public owner;
     string public dedicatedTo;
     string public content;
     
-    modifier restricted() {
-        require(msg.sender == owner);
-        _;
-    }
-    
-    constructor (address newOwner, string newDedicatedTo, string newContent) public {
-        owner = newOwner;
+    constructor (string newDedicatedTo, string newContent) public {
         dedicatedTo = newDedicatedTo;
         content = newContent;
     }
     
     function getDedication () public view returns (string, string) {
         return (dedicatedTo, content);
-    }
-    
-    function setDedicatedTo (string newDedicatedTo) public restricted {
-        dedicatedTo = newDedicatedTo;
-    }
-    
-    function setContent (string newContent) public restricted {
-        content = newContent;
     }
 }
 
@@ -34,8 +19,16 @@ contract DedicationFactory {
     mapping (address => address[]) public dedicationsByAddress;
     
     function newDedication (string dedicatedTo, string content) public {
-        address tempDedication = new Dedication(msg.sender, dedicatedTo, content);
+        address tempDedication = new Dedication(dedicatedTo, content);
         deployedDedications.push(tempDedication);
         dedicationsByAddress[msg.sender].push(tempDedication);
+    }
+
+    function getDeployedDedications() public view returns (address[]) {
+        return deployedDedications;
+    }
+
+    function getDedicationsByAddress(address lookupAddress) public view returns (address[]) {
+        return dedicationsByAddress[lookupAddress];
     }
 }
---
+++ test/dedication.test.js
@@ -0,0 +1,62 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const compiledFactory = require('../ethereum/build/DedicationFactory.json');
+const compiledDedication = require('../ethereum/build/Dedication.json');
+
+let accounts;
+let factory;
+let dedicationAddress;
+let dedication;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ from: accounts[0], gas: '1000000' });
+
+  await factory.methods.newDedication("test", "to a great test").send({
+    from: accounts[0],
+    gas: '1000000'
+  });
+
+  [dedicationAddress] = await factory.methods.getDeployedDedications().call();
+  dedication = await new web3.eth.Contract(
+    JSON.parse(compiledDedication.interface),
+    dedicationAddress
+  );
+});
+
+describe('Dedications', () => {
+  it('deploys a factory and a dedication', () => {
+    assert.ok(factory.options.address);
+    assert.ok(dedication.options.address);
+  });
+
+  it('initalizes a dedication', async () => {
+    const content = await dedication.methods.getDedication().call();
+    assert.equal(content[0], "test");
+    assert.equal(content[1], "to a great test");
+  });
+
+  it('returns dedications based on an address', async () => {
+    await factory.methods.newDedication("test2", "to a great test2").send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await factory.methods.newDedication("test3", "to a great test3").send({
+      from: accounts[1],
+      gas: '1000000'
+    });
+
+    const account0Dedications = await factory.methods.getDedicationsByAddress(accounts[0]).call();
+    const account1Dedications = await factory.methods.getDedicationsByAddress(accounts[1]).call();
+
+    assert.equal(account0Dedications.length, 2);
+    assert.equal(account1Dedications.length, 1);
+  });
+});
