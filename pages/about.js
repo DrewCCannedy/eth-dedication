1adaccab2835971e2febabbdd649faf3408b5366
Drew Cannedy <dxc142830@utdallas.edu>
2018-12-30 14:27:58 -0600

    fix errors in about page

--- pages/about.js
+++ pages/about.js
@@ -9,15 +9,16 @@ class DedicationAbout extends Component {
     return (
       <Layout>
         <h2>Dedication/Blockchain/Ethereum overview</h2>
-        <p>This website uses the Ethereum network to save dedication's to people. To make a dedication to someone, a person sends cryptocurrency from their online digital wallet to pay for the processing power it takes to save a dedication to the Ethereum blockchain. This website specifically uses a test network, so no actual money is being used. To try this out for yourself, follow the steps below!</p>
+        <p>This website uses the Ethereum network to save dedications to people. To make a dedication to someone, a person sends crypto-currency from their online digital wallet to pay for the processing power it takes to save a dedication to the Ethereum blockchain. This website specifically uses a test network, so no actual money is being used. To try this out for yourself, follow the steps below!</p>
         <h2>How to</h2>
         <List ordered>
           <List.Item>
             Get the MetaMask broswer extension <a href='https://metamask.io/'>here</a>
             <List.List>
               <List.Item>This is your digital wallet that will hold your digital currency</List.Item>
-              <List.Item>When you're done, click the fox icon in the upper left of your browser</List.Item>
-              <List.Item>Create an account, then find the button to save your address to your clipboard</List.Item>
+              <List.Item>When you're done, click the fox icon in the upper right of your browser</List.Item>
+              <List.Item>Create an account, then click on the dropdown that says 'MainNet' and change it to 'Rinkeby'</List.Item>
+              <List.Item>Find the button to save your address to your clipboard and press it</List.Item>
               <List.Item>Your address is how you can send and receive cryptocurrency</List.Item>
             </List.List>
           </List.Item>
